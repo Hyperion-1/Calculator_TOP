8b95a6676d24e96a267f81f5ba20daece2764b09
Brad <walker.brad26@gmail.com>
2021-11-18 19:03:43 -0600

    gen. updates

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//comment
+/*                              TODO
+*   0: Build the 4 basic calculator functions[add, subtract, multiply, divide] 
+*       testing to be done console with hard coded test cases at this stage
+*
+*   1: Create a function 'operate' that takes an operator and 2 numbers then 
+*       calls one of the above functions on the numbers
+*
+*   2: Create basic HTML UI
+*       2a: wire js to UI
+*
+*   --complete above, move from there
+*/
+
+
+console.log("hidden Hello World")
