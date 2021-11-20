5a1482fc458c4df5873569baaa7918bbf836a86f
Brad <walker.brad26@gmail.com>
2021-11-19 18:06:33 -0600

    logical functions mapped out, in progress

--- script.js
+++ script.js
@@ -1,15 +1,57 @@
-/*                              TODO
-*   0: Build the 4 basic calculator functions[add, subtract, multiply, divide] 
-*       testing to be done console with hard coded test cases at this stage
-*
-*   1: Create a function 'operate' that takes an operator and 2 numbers then 
-*       calls one of the above functions on the numbers
-*
-*   2: Create basic HTML UI
-*       2a: wire js to UI
-*
-*   --complete above, move from there
-*/
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previousOperand]');
+const currentOperandTextElement = document.querySelector('[currentOperand]');
+
+
+class Calculator {
+    constructor(previousOperandTextElement, currentOperandTextElement){
+        this.previousOperandTextElement = previousOperandTextElement
+        this.currentOperandTextElement = currentOperandTextElement
+        this.clear()
+    }//end constructor
+}//end Calculator
+
+//hook all variables to operate on the 'Calculator' object
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement);
+
+
+
+function clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined  
+}//end clear()
+
+function delete_(){
+
+}//end del()
+
+function appendNumber(number){
+    numberButtons.forEach (button=> {
+        button.addEventListener('click', () => {
+            calculator.appendNumber(button.innerHTML)
+            calculator.updateDisplay()
+        })
+    })
+}//end appendNumber()
+
+function chooseOperation(operaton){
+
+}//end chooseOperation
+
+
+function compute(){
+
+}//end compute
+
+function updateDisplay(){
+    this.currentOperandTextElement.innerHTML = this.currentOperand
+}//end updateDisplay
+
 
 
 console.log("hidden Hello World")
