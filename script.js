c9b31e1ddd2760e5b81da6b53ba68bef86ef128e
Brad <walker.brad26@gmail.com>
2021-11-20 20:38:39 -0600

    Added minor JS documentation and comments for readability

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
+/** 
+ * Below is the logic/code for a basic non-graphing, non-scientific calculator
+ * Class: Calculator * 
+*/
 class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
       this.previousOperandTextElement = previousOperandTextElement
       this.currentOperandTextElement = currentOperandTextElement
       this.clear()
-    }
+    }//end constructor
   
     clear() {
       this.currentOperand = ''
       this.previousOperand = ''
       this.operation = undefined
-    }
+    }//end clear()
   
     delete() {
       this.currentOperand = this.currentOperand.toString().slice(0, -1)
-    }
+    }//end delete()
   
     appendNumber(number) {
       if (number === '.' && this.currentOperand.includes('.')) return
       this.currentOperand = this.currentOperand.toString() + number.toString()
-    }
+    }//end appendNumber()
   
     chooseOperation(operation) {
       if (this.currentOperand === '') return
@@ -28,7 +32,7 @@ class Calculator {
       this.operation = operation
       this.previousOperand = this.currentOperand
       this.currentOperand = ''
-    }
+    }//end chooseOperation()
   
     compute() {
       let computation
@@ -54,7 +58,7 @@ class Calculator {
       this.currentOperand = computation
       this.operation = undefined
       this.previousOperand = ''
-    }
+    }//end compute()
   
     getDisplayNumber(number) {
       const stringNumber = number.toString()
@@ -71,7 +75,7 @@ class Calculator {
       } else {
         return integerDisplay
       }
-    }
+    }//end getDisplayNumber()
   
     updateDisplay() {
       this.currentOperandTextElement.innerText =
@@ -82,10 +86,13 @@ class Calculator {
       } else {
         this.previousOperandTextElement.innerText = ''
       }
-    }
-  }
+    }//end update display()
+  }//end class calculator
   
-  
+  /**
+   * Below is the HTML referenced values for calculation. Marked as 'const' as 
+   * the value of each button does not change (2==2, at all times after all).
+   */
   const numberButtons = document.querySelectorAll('[data-number]')
   const operationButtons = document.querySelectorAll('[data-operation]')
   const equalsButton = document.querySelector('[data-equals]')
